c99b0baee7a82e815f78b92350c2fc5cf7622125
Chaxx <77371536+China-Chaxx@users.noreply.github.com>
2024-05-12 22:19:47 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">高高举起 轻轻放下<br>捧在手心 拥在怀里<br>是玻璃<br>用身体感受   <br>它的棱棱角角   <br>用温度体会   <br>它的与世隔离   <br>揉碎了 捏紧了   <br>放胸腔里   <br>我的第二心      <br>                        -Chaxx</div>    
 </template>
 
 <style lang="scss" scoped>
